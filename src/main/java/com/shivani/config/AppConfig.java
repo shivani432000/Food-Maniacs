346735b5822830df6e7b21939ba2e40ab7e616ab
shivani <shivani.porwal01@gmail.com>
2024-02-24 08:59:22 +0530

    app configuration for spring security

---
+++ src/main/java/com/shivani/config/AppConfig.java
@@ -0,0 +1,56 @@
+package com.shivani.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		
+		http.sessionManagement(management -> management.sessionCreationPolicy(
+				SessionCreationPolicy.STATELESS))
+		.authorizeHttpRequests(
+				Authorize -> Authorize.requestMatchers("/api/**").authenticated().
+				anyRequest().permitAll())
+		.csrf(csrf->csrf.disable())
+		.cors(cors->cors.configurationSource(CorsConfigurationSource()))
+		.formLogin(Customizer.withDefaults());
+
+		return http.build();
+	}
+
+	private CorsConfigurationSource CorsConfigurationSource() {
+		// TODO Auto-generated method stub
+		return new CorsConfigurationSource()
+				{
+
+					@Override
+					public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+						CorsConfiguration cfg = new CorsConfiguration();
+						cfg.setAllowedOrigins(Collections.singletonList("*"));
+						cfg.setAllowedMethods(Collections.singletonList("*"));
+						cfg.setAllowedHeaders(Collections.singletonList("*"));
+						cfg.setExposedHeaders(Collections.singletonList("*"));
+						cfg.setMaxAge(3600L);
+						return cfg;
+					}
+			
+				};
+	}
+
+	
+
+}
